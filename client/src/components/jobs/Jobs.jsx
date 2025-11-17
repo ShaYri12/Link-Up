6376840de9e0de565527d8e4d2ab65ae823e0f33
ShaYri12 <sharjeelb60@gmail.com>
2025-11-18 04:09:53 +0500

    Done

---
+++ client/src/components/jobs/Jobs.jsx
@@ -0,0 +1,33 @@
+import React, { useContext, useState } from "react";
+import Job from "../job/Job";
+import "./jobs.scss";
+import { useQuery } from "@tanstack/react-query";
+
+import { DarkModeContext } from "../../context/darkModeContext";
+import { BASE_URL } from "../../axios";
+
+const Posts = ({}) => {
+  const { darkMode } = useContext(DarkModeContext);
+  const { isLoading, error, data } = useQuery(["jobs"], async () => {
+    const response = await fetch(`${BASE_URL}/jobs`);
+    if (!response.ok) {
+      throw new Error("Failed to fetch data");
+    }
+
+    const responseData = await response.json();
+    return responseData;
+  });
+
+  return (
+    <div className={`posts ${darkMode ? "text-light" : ""}`}>
+      <h3>Recent Jobs</h3>
+      {error
+        ? "Something went wrong!"
+        : isLoading
+        ? "loading"
+        : data.map((job) => <Job job={job} key={job._id} />)}
+    </div>
+  );
+};
+
+export default Posts;
---
+++ client/src/context/authContext.js
@@ -0,0 +1,34 @@
+import axios from "axios";
+import { createContext, useEffect, useState } from "react";
+import { BASE_URL } from "../axios";
+
+export const AuthContext = createContext();
+
+export const AuthContextProvider = ({ children }) => {
+  const [currentUser, setCurrentUser] = useState(
+    JSON.parse(localStorage.getItem("user")) || null
+  );
+
+  const login = async (inputs) => {
+    const res = await axios.post(`${BASE_URL}/auth/login`, inputs, {
+      withCredentials: true,
+    });
+
+    setCurrentUser(res.data);
+  };
+
+  const logout = () => {
+    localStorage.removeItem("user");
+    setCurrentUser(null);
+  };
+
+  useEffect(() => {
+    localStorage.setItem("user", JSON.stringify(currentUser));
+  }, [currentUser]);
+
+  return (
+    <AuthContext.Provider value={{ currentUser, login, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
